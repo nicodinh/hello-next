b821585fa16b87b014ca1bf01456334d915e5766
nicolas <nicolas@azari.fr>
2017-09-05 23:58:15 +0200

    Fetching data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
-import Layout from '../components/Layout.js'
+import Layout from '../components/Layout.js';
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
 
-export default () => (
+const Index = (props) => (
   <Layout>
-    <p>Hello Next.js</p>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
   </Layout>
 );
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await res.json();
+
+  console.log(`Show data fetched. Count: ${data.length}`);
+
+  return {
+    shows: data
+  };
+};
+
+export default Index;
+
+// const PostLink = (props) => (
+//   <li>
+//     <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+//       <a>{props.title}</a>
+//     </Link>
+//   </li>
+// );
+
+// export default () => (
+//   <Layout>
+//     <h1>My Blog</h1>
+//     <ul>
+//       <PostLink id="hello-nextjs" title="Hello Next.js"/>
+//       <PostLink id="learn-nextjs" title="Learn Next.js is awesome"/>
+//       <PostLink id="deploy-nextjs" title="Deploy apps with Zeit"/>
+//     </ul>
+//   </Layout>
+// );
